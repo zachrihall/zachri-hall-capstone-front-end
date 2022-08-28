a4fe0d37d16990d021baab37a11c64854a7380b5
zachrihall <hallzachri@gmail.com>
2022-08-28 19:45:22 -0400

    changed production endpoint to api and not client

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ const BASE_URL = "https://" + document.location.hostname;
 
 export const API_URL =
     process.env.NODE_ENV === 'production'
-        ? 'https://zachri-hall-cap-api.herokuapp.com'
+        ? 'https://zachri-hall-cap-client.herokuapp.com'
         : BASE_URL;
 
