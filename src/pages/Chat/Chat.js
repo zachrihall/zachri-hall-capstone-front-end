5755acf2fe18fc84b39472ee2e9dbf03e0dadeb2
zachrihall <hallzachri@gmail.com>
2022-08-28 19:24:01 -0400

    edited api url import path in chat.js

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -4,7 +4,7 @@ import React from 'react';
 import io, { connect } from 'socket.io-client';
 import Profile from '../Profile/Profile';
 import Message from '../../components/Message/Message';
-import { API_URL } from '../src' //ftgbnfbnfhn
+import { API_URL as BASE_URL} from '../../../src/config/index.js'
 
 
 class Chat extends React.Component {
