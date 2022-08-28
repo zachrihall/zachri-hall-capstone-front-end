d33ef8da1183f06f34db83136b9bdabe5a407d88
zachrihall <hallzachri@gmail.com>
2022-08-28 19:31:06 -0400

    changed http to https for production in config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://" + document.location.hostname + ":8080";
+const BASE_URL = "https://" + document.location.hostname + ":8080";
 
 export const API_URL =
     process.env.NODE_ENV === 'production'
