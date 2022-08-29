7b214c2a06183c8e7ad03b246213cfb43d62a1bc
zachrihall <hallzachri@gmail.com>
2022-08-28 21:13:39 -0400

    changed profile api_key req

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -35,7 +35,7 @@ class Profile extends Component {
             userInfo: userInfo,
             userPosts: userPostsRes.data,
             isLoading: false,
-            API_KEY: api_key_resp
+            API_KEY: api_key_resp.key
         });
 
     }
